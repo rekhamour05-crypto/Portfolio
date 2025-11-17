549f45ceeab8be0219de3b4a7e26e034d44ab165
rekha_rani <rekha.rani@con.purecs.com>
2025-11-17 17:02:24 +0530

    removed commented code

--- app/src/main/java/com/example/rekha/portfolioapp/presentation/holdings/screen/PortfolioScreen.kt
+++ app/src/main/java/com/example/rekha/portfolioapp/presentation/holdings/screen/PortfolioScreen.kt
@@ -1,21 +1,13 @@
 package com.example.rekha.portfolioapp.presentation.holdings.screen
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -27,14 +19,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.rekha.portfolioapp.domain.model.Holding
-import com.example.rekha.portfolioapp.presentation.holdings.HoldingsUiState
 import com.example.rekha.portfolioapp.presentation.holdings.viewmodel.PortfolioViewModel
 import com.example.rekha.portfolioapp.presentation.holdings.reusables.AppToolbar
 import com.example.rekha.portfolioapp.presentation.holdings.reusables.HoldingsList
@@ -135,12 +125,6 @@ fun HoldingsScreenPreview() {
         Holding(symbol = "GOOG", ltp = 134.1, avgPrice = 120.0, qty = 5, close = 135.0),
         Holding(symbol = "TSLA", ltp = 240.8, avgPrice = 230.0, qty = 4, close = 243.0)
     )
-
-    val previewState = HoldingsUiState(
-        holdings = fakeHoldings,
-        isLoading = false
-    )
-
     HoldingsTheme {
       PortfolioScreen()
     }
